4cd709e4de29afcb1b600607940b6c19f83e2dd8
Bastien <bas.dijkstra@student.hku.nl>
2019-01-25 23:02:26 +0100

    Point A to B

---
+++ Assets/Scripts/PointAtoB.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class PointAtoB : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public int cycleTime;
+
+    private NavMeshAgent nM;
+    private float timer;
+
+    private Vector3 currentTarget;
+    private Vector3 targetA;
+    private Vector3 targetB;
+
+    void Start()
+    {
+        nM = GetComponent<NavMeshAgent>();
+        targetA = transform.InverseTransformPoint(pointA.position);
+        targetB = transform.InverseTransformPoint(pointB.position);
+        currentTarget = targetA;
+    }
+
+    void FixedUpdate()
+    {
+        timer += 1f / 60f;
+
+        if (timer >= cycleTime)
+        {
+            timer = 0;
+            if (currentTarget != targetA)
+                currentTarget = targetA;
+            else
+                currentTarget = targetB;
+        }
+
+        nM.SetDestination(currentTarget);
+    }
+}
